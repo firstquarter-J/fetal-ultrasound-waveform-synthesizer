91cdd5e3f6840642474c76bf90ac0129b002f128
firstquarter-J <firstquarter.jang@gmail.com>
2025-11-11 17:11:19 +0900

    add: :art: Add src/ package structure

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Fetal Ultrasound Waveform Synthesizer
+
+태아 초음파 영상에서 심박 파형을 검출하고 심박음을 합성합니다.
+"""
+
+__version__ = "0.1.0"
+__author__ = "firstquarter-J"
---
+++ src/detection/__init__.py
@@ -0,0 +1,17 @@
+"""
+Waveform Detection Module
+
+초음파 영상에서 심박 파형을 자동으로 검출합니다.
+
+주요 기능:
+- Orange 파형 검출
+- Gray 파형 검출
+- Orange Fragmented 파형 검출
+- 배치 처리 지원
+"""
+
+# 향후 모듈화 시 주요 함수들을 export
+# from .analyzer import analyze_video
+# from .batch import batch_analyze
+
+# __all__ = ["analyze_video", "batch_analyze"]
